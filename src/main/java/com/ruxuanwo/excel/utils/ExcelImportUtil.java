244e15bb546898e730034522d9fe22dbb656288e
如漩涡 <1594915884@qq.com>
2018-08-21 11:15:57 +0800

    Update ExcelImportUtil.java

--- src/main/java/com/ruxuanwo/excel/utils/ExcelImportUtil.java
+++ src/main/java/com/ruxuanwo/excel/utils/ExcelImportUtil.java
@@ -1,11 +1,11 @@
 package com.ruxuanwo.excel.utils;
 
+import com.ruxuanwo.excel.excel.ExcelField;
 import com.ruxuanwo.excel.excel.ExcelSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +21,7 @@ import java.util.List;
 /**
  * Excel导入工具
  *
- * @author chenbin on 2017-09-12
+ * @author chenbin o
  * @version 1.1.0
  */
 public class ExcelImportUtil {
@@ -36,6 +36,8 @@ public class ExcelImportUtil {
      */
     public static List<Object> importExcel(Class<?> sheetClass, Workbook workbook) {
         try {
+            List<Object> msgList = new ArrayList<Object>();
+            String msg = null;
             // sheet
             ExcelSheet excelSheet = sheetClass.getAnnotation(ExcelSheet.class);
             String sheetName = (excelSheet!=null && excelSheet.name()!=null && excelSheet.name().trim().length()>0)?excelSheet.name().trim():sheetClass.getSimpleName();
@@ -47,32 +49,39 @@ public class ExcelImportUtil {
                     if (Modifier.isStatic(field.getModifiers())) {
                         continue;
                     }
-                    fields.add(field);
+                    ExcelField excelField = field.getAnnotation(ExcelField.class);
+                    if (excelField != null){
+
+                        fields.add(field);
+                    }
                 }
             }
 
-            if (fields==null || fields.size()==0) {
-                throw new RuntimeException(">>>>>>>>>>> xxl-excel error, data field can not be empty.");
-            }
-
+//            if (fields==null || fields.size() != 8) {
+//                throw new RuntimeException(">>>>>>>>>>> xxl-excel error, data field can not be empty.");
+//            }
             // sheet data
             Sheet sheet = workbook.getSheet(sheetName);
-
             Iterator<Row> sheetIterator = sheet.rowIterator();
             int rowIndex = 0;
             List<Object> dataList = new ArrayList<Object>();
             while (sheetIterator.hasNext()) {
                 Row rowX = sheetIterator.next();
+
                 if (rowIndex > 0) {
                     Object rowObj = sheetClass.newInstance();
                     for (int i = 0; i < fields.size(); i++) {
                         Field field = fields.get(i);
+                        rowX.getCell(i).setCellType(CellType.STRING);
                         String fieldValueStr = rowX.getCell(i).getStringCellValue();
-
+                        if (fieldValueStr == null || "null".equals(fieldValueStr) || "".equals(fieldValueStr)){
+                            ExcelField excelField = field.getAnnotation(ExcelField.class);
+                            throw new RuntimeException("第" + (rowIndex + 1) + "行" + excelField.name() + "不能为空");
+                        }
                         Object fieldValue = FieldReflectionUtil.parseValue(field, fieldValueStr);
-
                         field.setAccessible(true);
                         field.set(rowObj, fieldValue);
+
                     }
                     dataList.add(rowObj);
                 }
@@ -141,6 +150,17 @@ public class ExcelImportUtil {
             logger.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
+    }
+
+    public static Workbook getWorkBook(InputStream stream, String path) {
+        Workbook workbook = null;
+        try {
+            workbook = path.endsWith(".xls") ? (new HSSFWorkbook(stream))
+                    : (path.endsWith(".xlsx") ? (new XSSFWorkbook(stream)) : (null));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return workbook;
     }
 
 }
